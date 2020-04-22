9114cfb3ee1c28d339d866d0bb203c09d0aaa22f
Victor Mithamo <vickarungaru@gmail.com>
2020-04-22 20:44:32 +0300

    update to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'foundation-sites/dist/js/foundation.min.js';
+import 'what-input/dist/what-input.min.js';
+import 'motion-ui/dist/motion-ui.min.js';
 // import 'foundation-sites/dist/css/foundation.min.css';
 
 ReactDOM.render(
